b50745cbf57a11ba828ecec3833e8b28ed9818c5
daniely2701 <danielyefta4@gmail.com>
2024-05-09 19:46:49 +0700

    .

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Daniel Yefta
+            Saya tinggal di daerah Bekasi
+            olahraga yang saya sukai adalah Renang";
+    }
+}
